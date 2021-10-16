d597113ca9aa8bc4d9f018cc02713759d7bcec0a
badbadya66 <chichaya.marod@mail.kmutt.ac.th>
2021-10-16 23:38:56 +0700

    Exploratory

---
+++ CODE/Exploratory.R
@@ -0,0 +1,8 @@
+library("dplyr") 
+library("stringr") 
+library("tidyr") 
+library("assertive") 
+library("readr")  
+
+Athletes <- read.csv ("https://raw.githubusercontent.com/sit-2021-int214/029-2021-Olympics-in-Tokyo/master/Athletes.csv")
+View(Athletes)
